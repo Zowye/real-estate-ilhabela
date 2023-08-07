07b0d1c0b1de96693b1a6397dab288fca28e285b
Zowye <caiozowye@gmail.com>
2023-08-06 21:03:37 -0300

    Cards common info chip color font theme switchable

--- src/components/CardCommonInfo.vue
+++ src/components/CardCommonInfo.vue
@@ -1,73 +1,75 @@
 <template>
-    <div class="container1x">
-        <div class="item">
-            <div class="icon">
-                <i class="fas fa-bed"></i>
-            </div>
-            <div class="value">3</div>
-        </div>
-        <div class="item">
-            <div class="icon">
-                <i class="fas fa-car"></i>
-            </div>
-            <div class="value">2</div>
-        </div>
-        <div class="item">
-            <div class="icon">
-                <i class="fas fa-ruler-combined"></i>
-            </div>
-            <div class="value">120m²</div>
-        </div>
-        <div class="item">
-            <div class="icon">
-                <i class="fas fa-umbrella-beach"></i>
-            </div>
-            <div class="value">500m</div>
-        </div>
-        <div class="item">
-            <div class="icon">
-                <i class="fas fa-couch"></i>
-            </div>
-            <div class="value">Não</div>
-        </div>
+  <div class="container1x">
+    <div class="item">
+      <div class="icon">
+        <i class="fas fa-bed"></i>
+      </div>
+      <div class="value">3</div>
     </div>
+    <div class="item">
+      <div class="icon">
+        <i class="fas fa-car"></i>
+      </div>
+      <div class="value">2</div>
+    </div>
+    <div class="item">
+      <div class="icon">
+        <i class="fas fa-ruler-combined"></i>
+      </div>
+      <div class="value">120m²</div>
+    </div>
+    <div class="item">
+      <div class="icon">
+        <i class="fas fa-umbrella-beach"></i>
+      </div>
+      <div class="value">500m</div>
+    </div>
+    <div class="item">
+      <div class="icon">
+        <i class="fas fa-couch"></i>
+      </div>
+      <div class="value">Não</div>
+    </div>
+  </div>
 </template>
   
 <style scoped>
 .container1x {
-    margin:2.65em 0 0.65em 0;
-    font-size: 0.85em;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    width: 100%;
+  margin: 2.65em 0 0.65em 0;
+  font-size: 0.85em;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  width: 100%;
 }
 
 .item {
-    background-color:var(--chip-color);
-    display: flex;
-    margin-bottom: 10px;
-    margin-right: 0.35em;
-    padding: 0.25em 0.6em;
-    border-radius: 0.82em;
-    color: var(--cor-base);
-    /* border: 1px solid var(--cor-text-subbase); */
+  background-color: var(--chip-color);
+  display: flex;
+  margin-bottom: 10px;
+  margin-right: 0.35em;
+  padding: 0.25em 0.6em;
+  border-radius: 0.82em;
+  color: var(--cor-base);
+  /* border: 1px solid var(--cor-text-subbase); */
 }
 
 .icon {
-    margin-right: 0.30em;
-    font-size: 0.8em;
-    color: rgb(85, 85, 85);
-    border-radius: 50%;
+  margin-right: 0.30em;
+  font-size: 0.8em;
+  border-radius: 50%;
+  color: var(--black-or-white-inverse-theme);
 }
 
 .title {
-    font-weight: bold;
+  font-weight: bold;
 }
 
 .value {
-    margin-left: auto;
-    color:rgb(115, 109, 109);}
+  margin-left: auto;
+  color: var(--black-or-white-inverse-theme);
+  /* color: rgb(115, 109, 109); */
+}
 </style>
   
 
